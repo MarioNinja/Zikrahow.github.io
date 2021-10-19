2d7fb6658e8d80b8e5ca17030e14c3ae2c759f91
MarioNinja <92745284+MarioNinja@users.noreply.github.com>
2021-10-19 03:09:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function myFonction() {
+    var x = document.getElementById("mynavigateur");
+    if (x.className === "navigateur") {
+      x.className += " responsive";
+    } else {
+      x.className = "navigateur";
+    }
+  }
